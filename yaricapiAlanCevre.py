0db8eea6e205a0c9417db57545d21dc0c35e5657
Hasan Biter <98813125+Hasanbtr@users.noreply.github.com>
2023-11-19 15:41:14 +0300

    Add files via upload

---
+++ yaricapiAlanCevre.py
@@ -0,0 +1,16 @@
+#Uygulama: Yarı çapı verilen bir dairenin alan ve çevresini hesaplayınız.
+
+#Daire Alanı : πr² 
+
+#Daire Çevresi : 2πr 
+
+#pi: 3.14
+
+pi=3.14
+yaricap=input("yaricap :")
+alan=pi*(float(yaricap)**2)
+cevre=2*pi*float(yaricap)
+print("Alan :" +str(alan))
+print("Çevre :"+str(cevre))
+
+
